55f81b73d86a885d64e942f5e306fbc6e133b508
AirDur <ralphagris@gmail.com>
2019-05-14 16:26:37 +0200

    add SpringBootServletInitializer for App Starter to correcty run the app on CircleCI.

--- src/main/java/com/polytech/polyNet/AppStarter.java
+++ src/main/java/com/polytech/polyNet/AppStarter.java
@@ -2,9 +2,10 @@ package com.polytech.polyNet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppStarter {
+public class AppStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AppStarter.class);
     }
